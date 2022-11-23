4025c74a524b8f4add1ab88b9afbfd49b52f41b1
glebnar <kevinpicart@free.fr>
2022-11-23 16:09:32 +0100

    a

--- selfie-a-wookies/projects/mon-application/src/app/app-routing.module.ts
+++ selfie-a-wookies/projects/mon-application/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListSelfiesComponent } from './features/selfie/list-selfies/list-selfies.component';
+import { ListWookieComponent } from './features/wookie/list-wookie/list-wookie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'selfies',
+    component:ListSelfiesComponent
+  },
+  {
+    path:'wookies',
+    component:ListWookieComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
